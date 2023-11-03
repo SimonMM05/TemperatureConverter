562817ad33f8622c923368bd80df66b3ab3fbe8b
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-03 20:25:40 +0100

    Mere simpelt

--- Program.cs
+++ Program.cs
@@ -1,58 +1,52 @@
 ﻿using System;
 
-class Program
+Console.WriteLine("Hello! Welcome to this program. The program allows you to convert between Celsius and Fahrenheit.");
+while (true)
 {
-    static void Main(string[] args)
+    Console.WriteLine("Enter a number for a temperature you would like to convert to either Celsius or Fahrenheit (or 'q' to quit):");
+    string input = Console.ReadLine();
+
+    if (input == "q" || input == "quit" || input == "exit")
     {
-        Console.WriteLine("Hello! Welcome to this program. The program allows you to convert between Celsius and Fahrenheit.");
-        while (true)
+        break;
+    }
+
+    if (double.TryParse(input, out double temperature))
+    {
+        Console.WriteLine($"You entered: {temperature}");
+
+        Console.WriteLine("Select conversion: (1) Celsius to Fahrenheit, (2) Fahrenheit to Celsius");
+        int choice;
+        if (int.TryParse(Console.ReadLine(), out choice))
         {
-            Console.WriteLine("Enter the temperature you would like to convert (or 'q' to quit):");
-            string input = Console.ReadLine();
+            double convertedTemperature = 0.0;
+            string conversionType = "";
 
-            if (input == "q" || input == "quit" || input == "exit")
+            if (choice == 1)
             {
-                break;
+                convertedTemperature = (temperature * 9 / 5) + 32;
+                conversionType = "Celsius to Fahrenheit";
             }
-
-            if (double.TryParse(input, out double temperature))
+            else if (choice == 2)
             {
-                Console.WriteLine($"You entered: {temperature}");
-
-                Console.WriteLine("Select conversion: (1) Celsius to Fahrenheit, (2) Fahrenheit to Celsius");
-                int choice;
-                if (int.TryParse(Console.ReadLine(), out choice))
-                {
-                    double convertedTemperature = 0.0;
-                    string conversionType = "";
-
-                    if (choice == 1)
-                    {
-                        convertedTemperature = (temperature * 9 / 5) + 32;
-                        conversionType = "Celsius to Fahrenheit";
-                    }
-                    else if (choice == 2)
-                    {
-                        convertedTemperature = (temperature - 32) * 5 / 9;
-                        conversionType = "Fahrenheit to Celsius";
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid choice. Please select (1) or (2).");
-                        continue;
-                    }
-
-                    Console.WriteLine($"Converted temperature in {conversionType}: {convertedTemperature}");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid choice. Please select (1) or (2).");
-                }
+                convertedTemperature = (temperature - 32) * 5 / 9;
+                conversionType = "Fahrenheit to Celsius";
             }
             else
             {
-                Console.WriteLine("Invalid input. Please enter a valid number.");
+                Console.WriteLine("Invalid choice. Please select (1) or (2).");
+                continue;
             }
+
+            Console.WriteLine($"Converted temperature in {conversionType}: {convertedTemperature}");
         }
+        else
+        {
+            Console.WriteLine("Invalid choice. Please select (1) or (2).");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number.");
     }
 }
